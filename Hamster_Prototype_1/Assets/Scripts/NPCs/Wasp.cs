24ce8cae91b1e3f98dfc9a86577ab5ab327c49b0
Lucas-Burford <lucasburford@live.co.uk>
2021-11-03 19:42:14 +0000

    Added Wasp enemy

---
+++ Hamster_Prototype_1/Assets/Scripts/NPCs/Wasp.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wasp : MonoBehaviour
+{
+    #region Members
+    [Header("Wasp Values")]
+
+    [SerializeField]
+    // Wasp health
+    private float health;
+    public float Health
+    {
+        get { return health; }
+    }
+
+    [SerializeField]
+    // Wasp behaviour states
+    private enum States
+    {
+        idle,
+        attacking
+    }
+    [SerializeField]
+    private States state;
+
+    #endregion
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Initialise values
+        health = 100;
+        // Set starting state to idle
+        state = States.idle;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (state == States.idle)
+        {
+            Hover();
+        }
+    }
+
+    /// <summary>
+    /// Hover up and down
+    /// </summary>
+    private void Hover()
+    {
+        float y = Mathf.PingPong(Time.time, 1);
+
+        transform.position = new Vector3(transform.position.x, y, transform.position.z);
+    }
+}
